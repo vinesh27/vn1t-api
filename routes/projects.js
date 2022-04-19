cc9a0b68d58b83b0e522d4df146c98f5d9ff9c99
vinesh27 <vinitrajpurohit00@gmail.com>
2022-04-19 18:51:06 +0530

    Project API Deleted

--- routes/projects.js
+++ routes/projects.js
@@ -16,36 +16,4 @@ router.get("/getProjects", function (req, res, next) {
   });
 });
 
-router.post("/addProject", function (req, res, next) {
-  fs.readFile("./data/projects.json", "utf8", (err, data) => {
-    if (err) {
-      const obj = {
-        success: false,
-        message: "Error reading file",
-        fullError: err,
-      };
-      res.send(obj);
-    } else {
-      const obj = JSON.parse(data);
-      obj.push(req.body);
-      fs.writeFile("./data/projects.json", JSON.stringify(obj), (err) => {
-        if (err) {
-          const obj = {
-            success: false,
-            message: "Error writing file",
-            fullError: err,
-          };
-          res.send(obj);
-        } else {
-          const obj = {
-            success: true,
-            message: "Project added",
-          };
-          res.send(obj);
-        }
-      });
-    }
-  });
-});
-
 module.exports = router;
